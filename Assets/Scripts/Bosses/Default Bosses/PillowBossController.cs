79d3a129fa1a3f3b85d993d97870b9286bd95c87
LDMONTY007 <logan.d.m.117@gmail.com>
2025-03-18 14:29:06 -0400

    Implement Pillow boss
    TODO: Someone needs to implement the ground pound animation and attack where the boss slams on the ground and makes a wave that damages the player.

---
+++ Assets/Scripts/Bosses/Default Bosses/PillowBossController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PillowBossController : BossController
+{
+
+    public override void HandleStateMachine()
+    {
+
+
+        #region handling individual states
+
+        switch (curState)
+        {
+            case BossState.idle:
+                //bossRenderer.material.color = Color.white;
+                HandleIdle();
+                break;
+            case BossState.attack:
+                //bossRenderer.material.color = Color.red;
+                HandleAttack();
+                break;
+            case BossState.move:
+                //bossRenderer.material.color = Color.blue;
+                HandleMove();
+                break;
+            case BossState.stun:
+                //bossRenderer.material.color = Color.yellow;
+                break;
+        }
+
+        #endregion
+    }
+
+    public override void HandleAnimation()
+    {
+        //if our velocity is greater than zero,
+        //then do our moving animation,
+        //otherwise don't do our moving animation.
+        if (rb.linearVelocity.magnitude > 0)
+        {
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+}
