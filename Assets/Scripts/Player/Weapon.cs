66834e37b9aa103a2c4b04d9912742fabc7cf81c
LDMONTY007 <69697953+LDMONTY007@users.noreply.github.com>
2025-04-07 13:08:36 -0400

    Implemented the Alt air slam attack for the sword.
    TODO: Make it so bosses take knockback using the same code that the player uses for knockback so we can start getting combos off that way.

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -112,10 +112,20 @@ public class Weapon : Collectible
             return;
         }
 
-        
+        //TODO: 
+        //code a different attack that occurs when in the air. this way the player can have a quick downward smash attack that
+        //works only if they are in the air and do their alt attack with the sword.
+        if (player.inAir)
+        {
+            StartCoroutine(AirAltAttackCoroutine());
+        }
+        else
+        {
+            //Start AltAttackCoroutine
+            StartCoroutine(AltAttackCoroutine());
+        }
 
-        //Start AltAttackCoroutine
-        StartCoroutine(AltAttackCoroutine());
+        
     }
 
     public virtual IEnumerator AttackCoroutine()
@@ -242,11 +252,6 @@ public class Weapon : Collectible
         //don't allow other attacks during our current attack.
         canAttack = false;
 
-        //TODO:
-        //implement an animation for attacking,
-        //but for now enable the collider for an attack.
-        //weaponCollider.enabled = true;
-
         List<GameObject> objs = collisionSensor.ScanForObjects();
 
         if (objs.Count > 0)
@@ -305,34 +310,118 @@ public class Weapon : Collectible
             }
         }
 
-        /*//RaycastHit hits = Physics.SphereCastAll(transform.position, attackRadius, transform.forward, attackDistance, playerMask);
-        RaycastHit sphereHit;
+        //set color of debug mesh to show we are in cooldown
+        collisionSensor.sensorColor = cooldownMeshColor;
+
+        //if we have a cooldown, wait the cooldown time before attacking.
+        if (hasCooldown)
+            yield return new WaitForSeconds(cooldownTime);
+
+        //restore default color
+        collisionSensor.sensorColor = ogMeshColor;
+
+        //allow us to attack again.
+        canAttack = true;
+
+        yield break;
+    }
+
+    public IEnumerator AirAltAttackCoroutine()
+    {
         
-        if (Physics.SphereCast(transform.position, attackRadius, transform.forward * attackDistance, out sphereHit, attackDistance, playerMask))
+
+        //start the attack animation
+        if (animator != null)
         {
-            //if this hit is in front of us, check for damageables
-            if (Mathf.Abs(Vector3.Dot(transform.position, sphereHit.point)) > 0)
+            //tell the animator we are in the air.
+            animator.SetBool("inAir", true);
+
+            //do the alt attack animation.
+            animator.SetTrigger("altAttack");
+        }
+
+        //don't allow other attacks during our current attack.
+        canAttack = false;
+
+        //wait for fixedupdate before launching player.
+        //if we didn't wait we'd have inconsistent physics.
+        yield return new WaitForFixedUpdate();
+
+        player.LaunchPlayer(-player.transform.up, 30f, 1f, 2f);
+
+
+        //we need to wait for the player to land on the ground again
+        //before continuing.
+        while (!player.isGrounded)
+        {
+            yield return null;
+        }
+
+        //Say we are no longer in the air so that we do the ground landing
+        //sword animation
+        if (animator != null)
+        {
+            animator.SetBool("inAir", false);
+        }
+
+
+        List<GameObject> objs = collisionSensor.ScanForObjects();
+
+        if (objs.Count > 0)
+        {
+            for (int i = 0; i < objs.Count; i++)
             {
-                IDamageable damageable = sphereHit.collider.GetComponent<IDamageable>();
-                if (damageable != null)
+                if (objs[i] != null)
                 {
-                    //make the damageable take damage.
-                    //and tell it we gave it damage.
-                    damageable.TakeDamage(1, gameObject);
+                    IDamageable damageable = objs[i].GetComponent<IDamageable>();
+                    if (damageable != null)
+                    {
+                        //if the damageable is a boss, and is dead, skip this object in the loop.
+                        BossController boss = objs[i].GetComponent<BossController>();
+                        if (boss != null && boss.isDead)
+                        {
+                            continue;
+                        }
 
-                    //TODO:
-                    //Spawn a particle system burst that destroys itself
-                    //when we hit a damageable so that the player can see where they hit.
-                    //maybe give each weapon an individualized particle effect.
-                    //spawn a particle effect facing outward from the normal
-                    //at the position that was hit.
-                    Debug.Log(sphereHit.point);
-                    Instantiate(hitParticles, sphereHit.point, Quaternion.LookRotation(sphereHit.normal));
+                        //make the damageable take damage.
+                        //and tell it we gave it damage.
+                        //do 2 damage here so that using this attack combo is worth it,
+                        //because the player must first use the alt attack to get into the
+                        //air and then second must use it again while in the air to do the slam attack.
+                        damageable.TakeDamage(2, gameObject);
+
+                        //TODO:
+                        //Spawn a particle system burst that destroys itself
+                        //when we hit a damageable so that the player can see where they hit.
+                        //maybe give each weapon an individualized particle effect.
+                        //spawn a particle effect facing outward from the normal
+                        //at the position that was hit.
+                        //TODO:
+                        //in the future replace this with
+                        //some code that checks the direction
+                        //the attack is coming from, does a box 
+                        //cast and uses the normal and hit point from
+                        //that box cast to calculate where the damage
+                        //particle effect should spawn. 
+                        Collider c = objs[i].GetComponent<Collider>();
+
+
+                        Vector3 closestPoint = c.ClosestPoint(Camera.main.transform.position);
+
+
+                        Instantiate(hitParticles, closestPoint + (-Camera.main.transform.forward.normalized * 0.25f), Quaternion.LookRotation(Camera.main.transform.position));
+
+                        
+                    }
                 }
+                else
+                {
+                    Debug.LogWarning("Object was null while checking if it is damageable".Color("Orange"));
+                }
+
             }
+        }
 
-
-        }*/
 
         //set color of debug mesh to show we are in cooldown
         collisionSensor.sensorColor = cooldownMeshColor;
@@ -349,6 +438,7 @@ public class Weapon : Collectible
 
         yield break;
     }
+
 
     /*private void OnCollisionEnter(Collision collision)
     {
