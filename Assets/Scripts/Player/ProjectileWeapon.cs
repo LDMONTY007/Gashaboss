7afc7ac48cffbbfaa8ff8b76cc075340e9db1230
Shadowyish <Shadowyish0@gmail.com>
2025-05-06 01:03:42 -0400

    added specials and alts to projectile weapons

--- Assets/Scripts/Player/ProjectileWeapon.cs
+++ Assets/Scripts/Player/ProjectileWeapon.cs
@@ -6,6 +6,10 @@ public class ProjectileWeapon : Weapon
 {
     public GameObject projectile;
 
+    public float specialWaitTime = .5f;
+    
+    public float altSizeMult = 1.5f;
+
     //if the launch transform's forward is angled upwards it will launch upwards.
     public Transform launchTransform;
 
@@ -22,12 +26,62 @@ public class ProjectileWeapon : Weapon
         //Create projectile with the launch transform rotation at launch position
         Instantiate(projectile, launchTransform.position, launchTransform.rotation);
 
-
-        Projectile p = projectile.GetComponent<Projectile>();
-
         //if we have a cooldown, wait the cooldown time before attacking.
         if (hasCooldown)
             yield return new WaitForSeconds(cooldownTime);
+
+        //restore default color
+        //collisionSensor.sensorColor = ogMeshColor;
+
+        //allow us to attack again.
+        canAttack = true;
+
+        yield break;
+    }
+
+    public override IEnumerator AltAttackCoroutine(){
+        //don't allow other attacks during our current attack.
+        canAttack = false;
+
+        //Create projectile with the launch transform rotation at launch position
+        GameObject spawnedProj = Instantiate(projectile, launchTransform.position, launchTransform.rotation);
+
+        // Scale the size of the object up, for our alt attack
+        spawnedProj.transform.localScale *= altSizeMult;
+
+        //if we have a cooldown, wait the cooldown time before attacking.
+        if (hasCooldown)
+            yield return new WaitForSeconds(altCooldownTime);
+
+        //restore default color
+        //collisionSensor.sensorColor = ogMeshColor;
+
+        //allow us to attack again.
+        canAttack = true;
+
+        yield break;
+    }
+
+    public override IEnumerator SpecialAttackCoroutine()
+    {
+        //don't allow other attacks during our current attack.
+        canAttack = false;
+
+        //TODO:
+        //implement an animation for attacking,
+        //but for now enable the collider for an attack.
+        //weaponCollider.enabled = true;
+
+        //Create projectile with the launch transform rotation at launch position
+        Instantiate(projectile, launchTransform.position, launchTransform.rotation);
+
+        // For the Special we will fire two shots, in quick succession
+        yield return new WaitForSeconds(specialWaitTime);
+        Instantiate(projectile, launchTransform.position, launchTransform.rotation);
+
+        //if we have a cooldown, wait the cooldown time before attacking.
+        if (hasCooldown)
+            yield return new WaitForSeconds(specialCooldownTime);
 
         //restore default color
         //collisionSensor.sensorColor = ogMeshColor;
